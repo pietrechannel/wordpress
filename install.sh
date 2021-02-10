fa365031f06b6de6ebe2ced9205e15f04d2f81ce
pietrechannel <78876346+pietrechannel@users.noreply.github.com>
2021-02-10 20:32:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 apt update
 apt upgrade -y
-sudo apt install apache2 mariadb-server mariadb-client php libapache2-mod-php php-mysql
+sudo apt install apache2 mariadb-server mariadb-client php libapache2-mod-php php-mysql php-redis php-zip
+sudo mysql_secure_installation
